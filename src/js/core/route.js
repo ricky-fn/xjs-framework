8905e785db91ec6d33c2f7d0205fd8258028f2cb
Ricky <260488274@qq.com>
2018-06-14 18:07:59 +0800

    save

---
+++ src/js/core/route.js
@@ -0,0 +1,381 @@
+define('route', ['engine', 'tool'], function (xjs, tool) {
+    /**
+     * 路由模块
+     * @module route
+     */
+    function Router() {
+        this.state = {};
+        this.componentSequence = {};
+        this.map = [];
+        this._currentNexus = false;
+    }
+
+    // Router.prototype.definemap = {};
+
+    /**
+     * 定义路由映射表以及路由的回调函数，支持利用正则表达式
+     * @method setup
+     * @memberOf module:route
+     * @param [Object] routemap 路由定义表
+     * @param [Object] callbacks 回调组
+     * @example
+     * var router new Router();
+     * router.setup({
+     *   '#home': 'Page.Home' //请确保唯一性
+     * }，{
+     *   before: function(){}, //处理路由切换前触发
+     *   fail: function(){} //当路由找不到时的回调
+     * })
+     * router.define('Page.Home', function() {
+     *  //todo something
+     * })
+     * //同时支持使用正则表达式匹配路由
+     * //假设一个新闻业务模块包含bbc、cctv、netflix，那么路由可以这样定义:
+     * //#page/bbc/news/
+     * //#page/cctv/news/
+     * //#page/netflix/news/
+     * router.setup({
+     *  '#page/(\\w+)/news/': 'Page.News' // 可以匹配到上述三个新闻渠道
+     * });
+     * router.define('Page.News', function(nId) {
+     *   console.log(nid); //bbc
+     * });
+     */
+    Router.prototype.setup = function (routemap, cb) {
+        var rule;
+        this.callback = cb;
+        for (rule in routemap) {
+            if (!routemap.hasOwnProperty(rule)) continue;
+            this.map.push({
+                rule: new RegExp('^' + rule + '$', 'i'),
+                quote: routemap[rule]
+            });
+        }
+    };
+
+    /**
+     * 定义路由回调
+     * @method define
+     * @memberOf module:route
+     * @param {String} name
+     * @param {Object}[nexus] 此路由所依赖的关系链
+     * @param {Boolean}[authorize] 是否需要登录操作
+     * @param {Function} callback 回调函数，如果有路由关系链，将会等待关系链的成员准备好后再执行回调
+     * @example
+     * var router = new Router();
+     * //一般定义
+     * router.define('Page.Home', function() {
+     * //todo something
+     * })
+     * //需要登陆验证
+     * router.define('Page.Home', true, function() {
+     * //todo something
+     * });
+     * //需要路由依赖
+     * router.define('Page.Home', ['ui.Nav', 'ui.Header', 'ui.Footer'], function() {
+     * //todo something
+     * })；
+     * //需要路由依赖、登陆验证
+     * router.define('Page.Home', ['ui.Nav', 'ui.Header', 'ui.Footer'], true, function() {
+     * //todo something
+     * })；
+     */
+    // Router.prototype.define = function (name, nexus, authorize, cb) {
+    Router.prototype.define = function (config) {
+        var authorize = config.authorize || false;
+        var path = config.path;
+        var nexus = config.nexus;
+        var page = config.page;
+
+        if (page == undefined)
+            throw "please aim which one page are used to render for this path";
+
+        if (path == undefined)
+            throw "path is a necessary argument";
+
+        var self = this;
+        var components = {};
+
+        if (nexus) {
+            nexus.forEach(function (name) {
+                components[name] = self.componentSequence[name];
+            });
+        }
+
+        this.map.forEach(function (map) {
+            if (map.quote == path) {
+                map.authorize = authorize;
+                map.nexus = components;
+                map.page = page;
+                map.events = {
+                    beforeEnter: config.beforeEnter
+                };
+                return true;
+            }
+        });
+    };
+
+    /**
+     * 路由导航
+     * @method navigator
+     * @memberOf xjs/router
+     * @see module:route#navigator
+     */
+
+    /**
+     * 导航到下一个路由地址
+     * @method navigator
+     * @memberOf module:route
+     * @param {String}[hash] 下一个路由的Hash地址，不填写则默认引导到#home
+     * @param {Object}[state] 路由的缓存内容，将会存储到history.state对象里
+     * @param {Boolean}[replaceHash] 当设置为true时会提换历史记录里最后一条路由信息，当设置为false时则会以新增的方式插入历史纪录
+     * @example
+     * var router = new Router();
+     * //默认情况，引导到#home并在历史记录里插入新的记录
+     * router.navigator('#home'）;
+     * //追加路由缓存信息
+     * router.navigator('#home', {isHomePage: true});
+     * console.log(history.state.isHomePage) // true
+     * //提换当前路由历史记录
+     * router.navigator('#home', {}, true);
+     */
+    // Router.prototype.navigator = function (hash, state, replaceHash) {
+    Router.prototype.navigator = function (param) {
+        param = param || {};
+
+        var self = this,
+            to, from, currentRoute, route;
+
+        var url = tool.url();
+        currentRoute = matchRoute.call(this, url.hash);
+
+        route = matchRoute.call(this, param.path);
+
+        if (route == false)
+            throw "this path is not exist";
+
+        to = {
+            path: param.path,
+            param: param.path.match(route.rule).slice(1),
+            query: param.query
+        };
+
+        from = url.hash == param.path ? {} : {
+            path: url.hash,
+            param: url.hash.match(currentRoute.rule).slice(1),
+            query: url.query
+        };
+
+        if (route.events.beforeEnter) {
+            route.events.beforeEnter(to, from, function (next) {
+                if (next == to) {
+                    end();
+                } else {
+                    self.navigator(next);
+                }
+            });
+        } else {
+            end();
+        }
+
+        function end() {
+            history[param.replaceHash ? 'replaceState' : 'pushState'](null, null, '#' + to.path);
+
+            renderComponents.call(self, route).then(function () {
+                xjs.createView(route.page, {
+                    router: to
+                });
+            });
+        }
+    };
+
+    /**
+     * 启动路由监听事件，必须在定义路由引射表以及路由回调后再启动。<br>
+     * 将会监听路由切换事件，例如浏览器的回退和前进
+     * @method start
+     * @memberOf module:route
+     */
+    Router.prototype.start = function () {
+        var that = this;
+
+        function onHashChange(e) {
+            // var param = [];
+            var url = tool.url();
+            if (url.hash) {
+                // param.push(location.hash);
+                // if (e && e.isTrusted) {
+                //     param.push(null, true);
+                // }
+                // that.navigator.apply(that, param);
+                that.navigator({
+                    path: url.hash,
+                    query: url.query
+                })
+            } else {
+                that.navigator({
+                    path: 'home/'
+                });
+            }
+        }
+
+        window.onhashchange = onHashChange;
+        onHashChange();
+    };
+
+    Router.prototype.component = function (array) {
+        this.componentSequence = array;
+    };
+
+    /**
+     * 检查当前路由是否能在路由映射表里找到，如果找不到匹配值就会触发传递给setup里的fail函数
+     * @method checkMatchResult
+     * @memberOf module:route
+     * @param {String} hash
+     * @param {Object} router.map
+     */
+    function matchRoute(hash) {
+        var map = this.map;
+        var path = verify(hash, map);
+        var self = this;
+
+        if (!path)
+            return false;
+
+        if (path.authorize && !xjs.getUserInfo()) {
+            getAuthorization.call(self, hash);
+        } else {
+            return $.extend({}, path, {
+                param: hash.match(path.rule).slice(1)
+            })
+        }
+    }
+
+    /**
+     * 跳转到登陆模块，完成用户身份验证后再进入hash所匹配的路由
+     * @method getAuthorization
+     * @memberOf module:route
+     * @param hash
+     */
+    function getAuthorization(hash) {
+        this.navigator('#login/', {backHash: hash}, true);
+    }
+
+    /**
+     * 检测hash是否在路由路由映射表内
+     * @method verify
+     * @memberOf module:route
+     * @param hash
+     * @return {Boolean} 是否匹配到
+     */
+    function verify(hash, map) {
+        var path;
+        var hash = hash.indexOf('?') > 0 ? hash.slice(0, hash.indexOf('?')) : hash;
+        for (var obj in map) {
+            path = map[obj];
+
+            if (hash.match(path.rule))
+                return path;
+        }
+        return false;
+    }
+
+    /**
+     * 对当前路由依赖关系以及下一个路由依赖关系做对比，对不存在于路由关系链里的模块会销毁掉[xjs.destroyView]{@link xjs.destroyView}
+     * @method filterComponents
+     * @memberOf module:route
+     */
+    function filterComponents(nexus) {
+        var length = Object.keys(nexus);
+        var currentNexus = this._currentNexus;
+        if (length == 0) {
+            xjs.destroyView();
+            currentNexus = false;
+            return nexus;
+        } else {
+            if (!currentNexus) {
+                xjs.destroyView();
+                currentNexus = nexus;
+                return nexus;
+            } else {
+                debugger;
+                findAbandonedItem(nexus, currentNexus).forEach(function (key) {
+                    xjs.destroyView(currentNexus[key].id);
+                    delete currentNexus[key].id;
+                });
+
+                findAbandonedInstance(currentNexus).forEach(function (id) {
+                    xjs.destroyView(id);
+                });
+
+                this._currentNexus = nexus;
+
+                return findAddedItem(nexus, currentNexus);
+            }
+        }
+    }
+
+    function renderComponents(route) {
+        var renderTeam = filterComponents.call(this, route.nexus);
+        var renderSequence = [];
+        var self = this;
+
+        Object.keys(renderTeam).forEach(function (name) {
+            var def = $.Deferred();
+            renderSequence.push(def);
+            setTimeout(function() {
+                self.componentSequence[name].render(function (instance) {
+                    self.componentSequence[name].id = instance.id;
+                    def.resolve();
+                });
+            }, 0)
+        });
+
+        return $.when.apply(null, renderSequence);
+    }
+
+    function findAbandonedInstance(oldNexus) {
+        var ids = [];
+        var mark = false;
+        for (var obj in xjs._instances) {
+            mark = false;
+            for (var item in oldNexus) {
+                if (oldNexus[item].id == xjs._instances[obj].id)
+                    mark = true;
+            }
+            if (mark == false)
+                ids.push(xjs._instances[obj].id);
+        }
+
+        return ids;
+    }
+
+    function findAbandonedItem(nexus, oldNexus) {
+        var nexusArray = Object.keys(nexus);
+        var oldNexusArray = Object.keys(oldNexus);
+        var items = [];
+
+        oldNexusArray.forEach(function (key) {
+            if (nexusArray.indexOf(key) < 0) {
+                items.push(key);
+            }
+        });
+
+        return items;
+    }
+
+    function findAddedItem(nexus, oldNexus) {
+        var nexusArray = Object.keys(nexus);
+        var oldNexusArray = Object.keys(oldNexus);
+        var items = [];
+
+        nexusArray.forEach(function (key) {
+            if (oldNexusArray.indexOf(key) < 0) {
+                items.push(key);
+            }
+        });
+
+        return items;
+    }
+
+    return Router;
+});
---
+++ src/js/core/widget.js
@@ -0,0 +1,192 @@
+define('widget', ['engine', 'underscore', 'zepto'], function (xjs, _, $) {
+    var declare = xjs.declare;
+
+    /**
+     * @fileOverview 这是Page的基类，所有Page的默认事件和流程都是在这里被定义<br>
+     * 所有Page类通过[xjs.declare]{@link xjs.declare}申明，并将[widget]{@link widget}作为`parents`参数传入，用以继承默认事件流程。<br>
+     * 最后使用[xjs.createView]{@link xjs.createView}实例化Page类
+     *
+     * 所有基类的函数都可以被重写（除了`init`）,重写后的事件可以调用`this.super()`方法执行父类被覆盖的函数<br>
+     * widget的流程行顺序按照以下:<br>
+     * `init—>render—>request—>syncGetData—>buildRender—>startup—>onExit`
+     * @mixin widget
+     */
+    return declare('ui.Widget', {
+        /**
+         * Page类的初始化函数，同时控制渲染事件的执行流程，此方法不可以被重写。
+         * @memberOf widget
+         * @function init
+         * @param dom 根Dom节点，用于插入模板
+         */
+        init: function (dom, callback) {
+            this.domNode = this.domNode || (this.$domNode = $(dom)).get(0);
+
+            this.render();
+
+            $.when(this.syncGetData()).done(function () {
+                this.buildRender();
+                // if (!this.finalStep) {
+                //     xjs.broadcast.trigger('widgetReady', this.routeEventName);
+                // } else {
+                //     xjs.broadcast.trigger('allWidgetReady', this.routeEventName);
+                // }
+                /**
+                 * 当模板和数据都被渲染后就会调用startup事件，Page里的Dom节点操作以及业务逻辑都应该在这里实现。
+                 * @memberOf widget
+                 * @function startup
+                 */
+                this.startup && this.startup();
+                callback && callback();
+            }.bind(this));
+            return this;
+        },
+        render: function () {
+            /**
+             * 定义Page的标题
+             *
+             * @type {string}
+             * @memberOf widget
+             * @name title
+             */
+            document.title = this.title || document.title;
+            /**
+             * 实例化后的Page类id，此id是唯一的，可以用于`xjs.byId`获取到`This`对象
+             *
+             * @type {string}
+             * @memberOf widget
+             * @name id
+             */
+            this.id = this.domNode.id;
+        },
+        syncGetData: function () {
+            /**
+             * request函数用于设置需要预先请求的数据队列，所有队列请求成功后才会执行后面的流程。
+             * 对zepto的ajax模块进行了二次封装，所有参数和$.ajax一致。ajax返回的数据将会根据app的名字挂载到this.data下
+             * @memberOf widget
+             * @function request
+             * @example
+             * request: function() {
+             *     return {
+             *         app: 'name', //在request之后的函数中可以用this.data.name获取到数据
+             *         url: 'example.do',
+             *         data: {}
+             *     }
+             * }
+             * //或者
+             * request: function() {
+             *     return [{
+             *         app: 'app1', //在request之后的函数中可以用this.data.app1获取到数据
+             *         url: 'example.do',
+             *         data: {}
+             *     }, {
+             *         app: 'app2',
+             *         url: 'test.do'
+             *     }]
+             * }
+             */
+            var sequence = this.request ? this.request() : false;
+            var dtd = $.Deferred();
+
+            if (!sequence)
+                return dtd.resolve();
+
+            processSequence.call(this, dtd, sequence);
+
+            return dtd.promise();
+        },
+        /**
+         * 模板渲染流程，将会把this对象作为数据采集对象传入模板。并扫描模板里的自定义锚点后映射到this对象上<br>
+         * [data-xjs-element] 将挂载到this对象上，并通过$ + name 用以区分普通dom对象和jquery对象
+         * @example
+         * <div data-xjs-element="divNode"></div>
+         * //this.divNode 获取原始dom对象
+         * //this.$divNode 获取jquery对象
+         * @memberOf widget
+         * @function buildRender
+         * @see {widget#request}
+         */
+        buildRender: function () {
+            /**
+             * Page类的CSS Class Name
+             *
+             * @type {string}
+             * @memberOf widget
+             * @name baseClass
+             */
+            this.$domNode.addClass(this.baseClass);
+            /**
+             * 传入模板字符串，基于`underscore`的模板引擎渲染HTML
+             *
+             * @type {string}
+             * @memberOf widget
+             * @name templateString
+             */
+            if (this.templateString) {
+                this.domNode.innerHTML = _.template(this.templateString)(this);
+            }
+            __createNode.call(this) && __createEvent.call(this);
+        },
+        /**
+         * Page的退出事件，在路由切换被触发时调用，如果有添加事件监听需要自行注销，应该写在这个事件里，
+         * 如果你复写了这个函数，别忘了在function末尾调用this._super()
+         * @memberOf widget
+         * @function onExit
+         */
+        onExit: function () {
+            this.$domNode.off().remove();
+        }
+    });
+
+    function __createNode() {
+        var doms, dom, parents, n, i;
+        doms = this.domNode.querySelectorAll('[data-xjs-element]');
+        doms = Array.prototype.slice.call(doms);
+        for (i = 0; i < doms.length; i++) {
+            dom = $(doms[i]);
+            parents = dom.parents('[data-xjs-mixin]');
+            if (parents.length && parents[0] != this.domNode) break;
+            n = dom.data('xjs-element');
+            this[n] = ( this['$' + n] = dom ).get(0);
+        }
+        return true;
+    }
+
+    function __createEvent() {
+        var doms, dom, parents, n, i;
+        doms = this.domNode.querySelectorAll('[data-xjs-event]');
+        doms = Array.prototype.slice.call(doms);
+        if (this.$domNode.data('xjs-event')) doms.push(this.domNode);
+        for (i = 0; i < doms.length; i++) {
+            var fns = {}, f, j;
+            dom = $(doms[i]);
+            parents = dom.parents('[data-xjs-mixin]');
+
+            if (parents.length && parents[0] != this.domNode)
+                break;
+
+            n = dom.data('xjs-event');
+            f = n.replace(/\s/g, "").split(';').slice(0, -1);
+            for (j = 0; j < f.length; j++) {
+                var event = f[j].split(':');
+                dom.on(event[0], this[event[1]].bind(this));
+            }
+        }
+        return true;
+    }
+
+    function processSequence(dtd, param) {
+        var param = param instanceof Array ? param : [param], i, name, count = 0;
+        this.data = this.data || {};
+        for (i = 0; i < param.length; i++) {
+            name = param[i].app;
+            if (!param[i].hasOwnProperty('showShadow')) param[i].showShadow = true;
+            delete param[i].app;
+
+            xjs.load(param[i]).then(function(key, result) {
+                this.data[key] = reslut;
+                count += 1;
+                if (count == param.length) dtd.resolve();
+            }.bind(this, name));
+        }
+    }
+});
